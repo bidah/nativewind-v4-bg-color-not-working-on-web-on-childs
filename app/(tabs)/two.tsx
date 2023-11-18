73990714eed61ec6367013f872ed9ef2eb97fa95
rofi <rfigueroa@truelogic.io>
2023-11-18 17:47:39 -0300

    not working bg  on web

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 export default function TabTwoScreen() {
   return (
     <View className="bg-gray-100 flex-1 p-3">
-      <View className="flex-1 bg-white rounded-xl pt-6 items-center justify-center">
-        <Text className="text-2xl uppercase font-bold text-gray-700 text-center">
+      <View className="flex-1 bg-indigo-500 rounded-xl pt-6 items-center justify-center">
+        <Text className="text-2xl uppercase font-bold text-white text-center">
           Tab two
         </Text>
       </View>
